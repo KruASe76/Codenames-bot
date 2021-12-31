0411fb072cf97eb100f470dffee2c43dd4a82cae
KruASe <kruanse@ya.ru>
2021-12-31 16:25:07 +0300

    1.0 global bugfix

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ def get_prefix(bot, message):
         prefix = cursor.fetchone()[0]
         res = (prefix, "cdn") if prefix else ("cdn",)
     else:
-        res = ("cdn", "!",  "/", "-")
+        res = ("cdn", "!", "/", "-")
     
     return commands.when_mentioned_or(*res)(bot, message)
 
